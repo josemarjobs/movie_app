2f87138973c0db4903b1fe94277b670621d8025e
Josemar Magalhaes <josemarmagalhaes@hotmail.com>
2016-12-18 18:02:28 -0800

    Fetch actor based on the ActorName list

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"io"
 	"os"
-	"strings"
+	"sync"
 )
 
 var ActorNames []string
@@ -14,7 +14,20 @@ func Run(in stringReader, out io.Writer) {
 	ActorNames = []string{}
 	AskForNames(in)
 	fmt.Fprintf(out, "You selected the following %d actors:\n", len(ActorNames))
-	fmt.Fprintln(out, strings.Join(ActorNames, "\n"))
+
+	var wg sync.WaitGroup
+	for _, name := range ActorNames {
+		wg.Add(1)
+		go func(n string) {
+			defer wg.Done()
+			actor, err := FetchActor(n)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Fprintln(out, actor.Name)
+		}(name)
+	}
+	wg.Wait()
 }
 
 func main() {
